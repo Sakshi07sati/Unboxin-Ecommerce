692ec782037e054092aed1ce7b2260010c8d4d8e
sakshi patel <patelsakshi0716@gmail.com>
2026-04-08 18:01:47 -0500

    Make initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-2xl">Working</h1>
+    </div>
+  );
+}
+
+export default App;
